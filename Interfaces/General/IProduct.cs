b9377b21dbcaa976995c0e4d90eb30372e5a8677
Melvin Solano <melvingilberto@gmail.com>
2019-10-25 15:45:06 -0600

    Product interface, model, controller

--- Interfaces/General/IProduct.cs
+++ Interfaces/General/IProduct.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using BecodingDesktop.Models;
+using BecodingDesktop.Models.Catalogs;
+using BecodingDesktop.Models.General;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace BecodingDesktop.Interfaces.General
 {
     public interface IProduct
     {
-        List<Control> CreateView();
+        List<ProductModel> GetProducts();
+        MessageModel SetItem(ProductModel data);
+        MessageModel UpdateItem(ProductModel data);
+
+        MessageModel UpdateStateItem(ProductModel data);
+
+        Func<DataRow, ProductModel> GetMapper();
     }
 }
