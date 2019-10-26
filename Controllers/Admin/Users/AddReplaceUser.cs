653c58fd578b9a6bd219660daf01890ccb42c367
Humberto Antonio Galdamez Chavez <hertzchavez@gmail.com>
2019-10-26 14:57:48 -0600

    Solventando inconveniente en agregar usuario

--- Controllers/Admin/Users/AddReplaceUser.cs
+++ Controllers/Admin/Users/AddReplaceUser.cs
@@ -85,7 +85,6 @@ namespace BecodingDesktop.Controllers.Admin.Users
             var index = roles.FindIndex(r => r.Name.Equals((string.IsNullOrEmpty(user?.RoleText))?string.Empty:user?.RoleText));
             cmbRoles.SelectedIndex = index;
             controls.Add(cmbRoles);
-
             return controls;
         }
     }
