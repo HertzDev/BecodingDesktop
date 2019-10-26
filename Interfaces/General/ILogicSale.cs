653a71eb537c514231c7513c9d03f06d6a9bd5e3
Melvin Solano <melvingilberto@gmail.com>
2019-10-26 01:41:06 -0600

    Logica de ventas

---
+++ Interfaces/General/ILogicSale.cs
@@ -0,0 +1,27 @@
+﻿using BecodingDesktop.Models;
+using BecodingDesktop.Models.Catalogs;
+using BecodingDesktop.Models.General;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BecodingDesktop.Interfaces.General
+{
+    public interface ILogicSale
+    {
+
+        List<Control> CreateView();
+        List<SaleModel> GetProducts();
+        MessageModel SetItem(SaleModel data);
+
+        MessageModel UpdateStateItem(SaleModel data);
+
+        Func<DataRow, SaleModel> GetMapper();
+
+
+    }
+}
---
+++ Models/General/SaleModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BecodingDesktop.Models.General
+{
+    public class SaleModel
+    {
+
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string NFactura { get; set; }
+        public decimal Total { get; set; }
+        public decimal SubTotal { get; set; }
+        public int Canceled { get; set; }
+        public string DateCanceled { get; set; }
+        public int PaymentMethod { get; set; }
+        public int UserCanceled { get; set; }
+        public int UserInvoice { get; set; }
+        public string Nit { get; set; }
+        public string Building { get; set; }
+        public int InvoiceType { get; set; }
+        public string CreationDate { get; set; }
+        public string UpdateDate { get; set; }
+
+    }
+}
